774d1e43030f7060a6d23daa2b720033af95cb25
Edget2000 <Edget2000@yahoo.co.uk>
2016-05-26 13:45:39 +0100

    importing project to github

---
+++ src/main/java/com/demo/dvla/App.java
@@ -0,0 +1,13 @@
+package com.demo.dvla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
