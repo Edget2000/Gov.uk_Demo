fcf2ea5fdc8df01dfb438938df8af7242d502748
Edget2000 <Edget2000@yahoo.co.uk>
2016-05-26 14:50:07 +0100

    # WARNING: head commit changed in the meantime
    Added Selenium dependencies and first test

---
+++ src/main/java/com/demo/dvla/GovUkLandingPage.java
@@ -0,0 +1,25 @@
+package com.demo.dvla;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GovUkLandingPage {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+}
